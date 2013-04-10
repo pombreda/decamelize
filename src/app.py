904da51f86d0ad1b4e80a13d3cd60311a5def6ac
derwolfe <chriswwolfe@gmail.com>
2013-04-09 22:00:15 -0500

    having trouble with kwords True/False

--- src/app.py
+++ src/app.py
@@ -4,36 +4,29 @@ def convert(word):
     """Take a CamelCased word and return it as un_camel_cased
     e.g. convert(myWord) returns my_word
     """
+    if word == 'True' or word == 'False':
+        return word
+    
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', word) 
-        # this is counting  my=myFolder?
     s2 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1)
+    
     if '_' in s2:
         return s2.lower()
     return s2
 
-# some sort of matching system should be setup, such that you can find
-# it should be a rules based approach that breaks down a word.
-# if the word is something like "db.model" then it should be split on the 
-# period to be ["db", ".", "model"]
-# the words should be split so that only WORDs are handled not spaces
 
 def convert_with_rules(word):
     """ Words containing a certain symbol need to handled in different ways.
-        Convert_with_rules does by using a set of predefined rules dictate
-        how a word will be handled.
-
-        >>> convert_with_rules('db.model')
-        'db.model'
-        >>> convert_with_rules('myTest=myJudge')
-        'my_test=my_judge'
+    Convert_with_rules does by using a set of predefined rules dictate
+    how a word will be handled.
     """
 
-    symbols = ['=', '+', '-', ',']
+    symbols = ('=', '+', '-', ',')
     
     # these could be namespaces. Leave them alone. 
     if '.' in word: 
         return word
-
+    
     for symbol in symbols:
         if symbol in word:
             word = convert_symbol(word, symbol)
@@ -43,25 +36,24 @@ def convert_with_rules(word):
 
 def convert_symbol(word, symbol):
     words = word.split(symbol)
+    print(words)
     return symbol.join([convert(w) for w in words])
 
 def convert_line(line):
-    """Break the file into lines and convert it"""
+    """Convert the line for the file."""
     words = line.split(' ')
-    return ' '.join([convert_with_rules(word) for word in words]) + '\n'
+    return ' '.join([convert_with_rules(word) for word in words])
 
-def split_file(file_handle):
-    """Break the file into a set of lines"""
-    return file_handle.split('\n')
+#def split_file(file_handle):
+#    """Break the file into a set of lines"""
+#    return file_handle.split('\n')
 
-#def convert_file(file_handle):
-#    """Given an open file handle, create a new converted file and save it 
-#   with the extension .converted
-#    appended to the filename.
-#    """
-#    # could just replace the old file. 
-#    new_file = filename + '.converted'
-#    for line in f:
-#            print(convert(line))
-    
+def convert_file(filename):
+    """ Given a file handle, convert the file from camel case to underscore"""
+    # currently will just print out changes?
+    ext = '.converted'
+    with open(filename, 'r') as f:
+        with open(filename + ext, 'w') as out:
+            for line in f:
+                out.write(convert_line(line))
 
--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-from .app import convert, convert_with_rules, convert_line
+from .app import convert, convert_with_rules, convert_line, convert_file
 
 # test the convert function
 def test_convert():
@@ -6,13 +6,22 @@ def test_convert():
 
 def test_convert_with_noise():
     """convert one word in a sentance of three words"""
-    assert convert_line('my folder letsChange') == 'my folder lets_change\n'
+    assert convert_line('my folder letsChange') == 'my folder lets_change'
 
 def test_sentance_with_rules():
-    assert convert_line('judgeJudy = db.Model') == 'judge_judy = db.Model\n'
+    assert convert_line('judgeJudy = db.Model') == 'judge_judy = db.Model'
 
 def test_convert_with_rules_skips():
     assert convert_with_rules('db.Model') == 'db.Model'
 
+def test_convert_with_rules_equals():
+    assert convert_with_rules('model=test') == 'model=test'
 
+def test_convert_with_rules_kwords():
+    assert convert_with_rules('model=True') == 'model=True'
+
+
+#def test_long_string():
+#    convert_file('test_data/data.py')
+#    assert True
 
